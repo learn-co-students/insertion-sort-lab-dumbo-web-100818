aceba25db0befa5ed8172df455fc7c8880894482
github username <github email address>
2019-02-20 14:45:36 -0500

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 function findMinAndRemove(array){
+  let min = array[0]
+  let minI = 0
 
+  for(let i = 0; i < array.length; i++) {
+    if (array[i] < min) {
+      min = array[i]
+      minI = i
+    }
+
+  }
+  array.splice(minI, 1)
+  return min
 }
 
 function insertionSort(array){
+  let news
+  let sorts = []
+
+  while (array.length != 0) {
+    news = findMinAndRemove(array)
+    sorts.push(news)
+  }
+  return sorts
 
 }
