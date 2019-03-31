0864de9f14ca66ae33fa78149b34ad56fde5120e
Alex Chao <chao.alexander.123@gmail.com>
2019-03-31 21:56:51 +0000

    create insertionSort function to pass tests

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 function findMinAndRemove(array){
-
+  let minIdx = 0
+  let min = array[minIdx]
+  for(let i = 1; i < array.length; i++) {
+    if (array[i] < min) {
+      minIdx = i
+      min = array[i]
+    }
+  }
+  array.splice(minIdx, 1)
+  return min
 }
 
 function insertionSort(array){
-
+  let newMin;
+  let sorted = []
+  while(array.length != 0){
+    newMin = findMinAndRemove(array)
+    sorted.push(newMin)
+  }
+  return sorted;
 }
